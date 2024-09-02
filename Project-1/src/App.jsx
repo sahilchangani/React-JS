315133ffe6e944e0259e538b74e5a8d5735197c7
sahilchangani <162560844+sahilchangani@users.noreply.github.com>
2024-09-02 13:43:26 +0530

    Project-1

---
+++ Project-1/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from './Components/Header'
+import Page1 from './Components/Page1'
+import Second from './Components/Second'
+import Thied from './Components/Thied'
+import Footer from './Components/Footer'
+
+export default function App() {
+  return (
+    <div>
+      <Header/>
+      <Page1/>
+      <Second/>
+      <Thied/>
+      <Footer/>
+    </div>
+  )
+}
