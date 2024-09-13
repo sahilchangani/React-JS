61d1d2761e0f7c675f2c2e25c0d63c85dae26693
sahilchangani <162560844+sahilchangani@users.noreply.github.com>
2024-09-13 13:53:54 +0530

    Control&uncntrol

---
+++ Control&unControl/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Control from './Components/Control'
+import Uncontrol from './Components/Uncontrol'
+
+export default function App() {
+  return (
+    <div>
+      <Control/>
+      <Uncontrol/>
+    </div>
+  )
+}
