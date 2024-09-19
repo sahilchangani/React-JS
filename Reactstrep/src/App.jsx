84e5ee74d89b4e024fba2dbf7782867928fd141c
sahilchangani <162560844+sahilchangani@users.noreply.github.com>
2024-09-19 13:52:20 +0530

    Reactstrep

---
+++ Reactstrep/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Reactstrep from './Components/Reactstrep'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function App() {
+  return (
+    <div>
+      <Reactstrep/>
+    </div>
+  )
+}
