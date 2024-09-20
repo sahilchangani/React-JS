5e2da72025f64181eda2b9d5595d6f399297db91
sahilchangani <162560844+sahilchangani@users.noreply.github.com>
2024-09-20 13:37:11 +0530

    Reactstrep

---
+++ Reactstrep/src/Components/Reactstrep.jsx
@@ -0,0 +1,127 @@
+import React, { useState } from 'react';
+import { Card, CardBody, CardTitle, CardSubtitle, CardText, Button } from 'reactstrap';
+
+import {
+  Nav,
+  NavItem,
+  Dropdown,
+  DropdownItem,
+  DropdownToggle,
+  DropdownMenu,
+  NavLink,
+} from 'reactstrap';
+import {
+  Accordion,
+  AccordionBody,
+  AccordionHeader,
+  AccordionItem,
+} from 'reactstrap';
+
+export default function Reactstrep() {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const toggle = () => setDropdownOpen(!dropdownOpen);
+
+  const [open, setOpen] = useState('1');
+  const toggle1 = (id) => {
+    if (open === id) {
+      setOpen(null); // Explicitly set to null when closing
+    } else {
+      setOpen(id); // Open the clicked accordion item
+    }
+  };
+
+  return (
+    <div>
+      <Nav tabs>
+        <NavItem>
+          <NavLink href="#" active>
+            Link
+          </NavLink>
+        </NavItem>
+        <Dropdown nav isOpen={dropdownOpen} toggle={toggle}>
+          <DropdownToggle nav caret>
+            Dropdown
+          </DropdownToggle>
+          <DropdownMenu>
+            <DropdownItem header>Header</DropdownItem>
+            <DropdownItem disabled>Action</DropdownItem>
+            <DropdownItem>Another Action</DropdownItem>
+            <DropdownItem divider />
+            <DropdownItem>Another Action</DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+        <NavItem>
+          <NavLink href="#">Link</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="#">Another Link</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink disabled href="#">
+            Disabled Link
+          </NavLink>
+        </NavItem>
+      </Nav> <br /><br /><br />
+
+      <Accordion open={open} toggle={toggle1}>
+        <AccordionItem>
+          <AccordionHeader targetId="1">Accordion Item 1</AccordionHeader>
+          <AccordionBody accordionId="1">
+            <strong>This is the first item's accordion body.</strong>
+            You can modify any of this with custom CSS or overriding our default
+            variables. It's also worth noting that just about any HTML can
+            go within the <code>.accordion-body</code>, though the transition
+            does limit overflow.
+          </AccordionBody>
+        </AccordionItem>
+        <AccordionItem>
+          <AccordionHeader targetId="2">Accordion Item 2</AccordionHeader>
+          <AccordionBody accordionId="2">
+            <strong>This is the second item's accordion body.</strong>
+            You can modify any of this with custom CSS or overriding our default
+            variables. It's also worth noting that just about any HTML can
+            go within the <code>.accordion-body</code>, though the transition
+            does limit overflow.
+          </AccordionBody>
+        </AccordionItem>
+        <AccordionItem>
+          <AccordionHeader targetId="3">Accordion Item 3</AccordionHeader>
+          <AccordionBody accordionId="3">
+            <strong>This is the third item's accordion body.</strong>
+            You can modify any of this with custom CSS or overriding our default
+            variables. It's also worth noting that just about any HTML can
+            go within the <code>.accordion-body</code>, though the transition
+            does limit overflow.
+          </AccordionBody>
+        </AccordionItem>
+      </Accordion> <br /><br /><br />
+      <Card
+  style={{
+    width: '18rem'
+  }}
+>
+  <img
+    alt="Sample"
+    src="https://picsum.photos/300/200"
+  />
+  <CardBody>
+    <CardTitle tag="h5">
+      Card title
+    </CardTitle>
+    <CardSubtitle
+      className="mb-2 text-muted"
+      tag="h6"
+    >
+      Card subtitle
+    </CardSubtitle>
+    <CardText>
+      Some quick example text to build on the card title and make up the bulk of the card‘s content.
+    </CardText>
+    <Button>
+      Button
+    </Button>
+  </CardBody>
+</Card>
+    </div>
+  );
+}
